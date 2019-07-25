f58f189e994e25fbead54fa634f3a7f57706f333
gahamel <gaetanhamelpro@gmail.com>
2019-07-25 12:58:49 +0200

    add routing, connect mongoose and start authentication

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const {check, validationResult} = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req,res) => {
-    res.send('Register a user');
-});
+
+router.post(
+    '/',
+    [    
+        check('name','name is required')
+            .not()
+            .isEmpty(),
+        check('email','a valid email is required')
+            .isEmail(),
+        check('password','Please enter a password with 6 or ore characters')
+            .isLength({min:6})],
+    async (req,res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({erros:errors.array()});
+        }
+        const {name, email, password} = req.body;
+
+        try {
+            let user = await User.findOne({email}); // mongoose method findOne, findOne({key:value})
+            if(user) {
+                return res.status(400).json({msg:'User already exists'});
+            }
+
+            user = new User({
+                name,
+                email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            res.send('User Saved!')
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+);
 
 module.exports = router;
