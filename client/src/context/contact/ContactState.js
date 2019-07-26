37d52a7a574d8d0c80ab77c977241e01c7dec451
gahamel <gaetanhamelpro@gmail.com>
2019-07-26 16:12:51 +0200

    added update delete and filter functions + effects

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,31 +36,54 @@ const ContactState = props => {
             phone:'333-333-3333',
             type:'professional'
           },
-        ]
+        ],
+        current: null,
+        filtered:null
     };
   const [state,dispatch] = useReducer(contactReducer, initialState);
   // Add Contact
-    const addContact = contact => {
-      contact.id = uuid.v4();
-      dispatch({type: ADD_CONTACT, payload: contact});
-    }
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({type: ADD_CONTACT, payload: contact});
+  }
   // Delete Contact
-
+  const deleteContact = id => {
+    dispatch({type: DELETE_CONTACT, payload: id});
+  }
   // Set Current Contact
-
+  const setCurrent = contact => {
+    dispatch({type: SET_CURRENT, payload: contact});
+  }
   // Clear Current Contact
-
+  const clearCurrent = () => {
+    dispatch({type: CLEAR_CURRENT});
+  }
   // Update Contact
-
+  const updateContact = contact => {
+    dispatch({type: UPDATE_CONTACT, payload: contact});
+  }
   // Filer Contacts
-
+  const filterContacts = text => {
+    dispatch({type: FILTER_CONTACTS, payload:text});
+  }
   //Clear filter
-
+  const clearFilter = () => {
+    dispatch({type: CLEAR_FILTER});
+  }
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        current:state.current,
+        filtered:state.filtered,
+        addContact,
+        deleteContact,
+        clearCurrent,
+        setCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter
+
       }}
       >
         {props.children}
