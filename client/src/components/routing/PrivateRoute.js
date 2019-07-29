4c86dd4764e0bb19f2a5f50b771349d0d4eba5dd
gahamel <gaetanhamelpro@gmail.com>
2019-07-29 08:52:42 +0200

    fix typing errors

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,9 +1,9 @@
 import React, {useContext} from 'react';
-import {route, Redirect} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
 
 const PrivateRoute = ({component: Component, ...rest}) => {
-  const authContext = sueCotnext(AuthContext);
+  const authContext = useContext(AuthContext);
   const {isAuthenticated, loading}= authContext;
   return (
     <Route {...rest} render={props => !isAuthenticated && !loading ? (
